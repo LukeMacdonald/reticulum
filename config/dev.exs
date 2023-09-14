36f60a52184fcea15a46bd39364a714900becbd3
LukeMacdonald <lukemacdonald21@gmail.com>
2023-09-14 11:02:47 +1000

    editted certifacte details

--- config/dev.exs
+++ config/dev.exs
@@ -23,8 +23,8 @@ config :ret, RetWeb.Endpoint,
     port: 4000,
     otp_app: :ret,
     cipher_suite: :strong,
-    keyfile: "#{File.cwd!()}/priv/dev-ssl.key",
-    certfile: "#{File.cwd!()}/priv/dev-ssl.cert"
+    keyfile: "#{File.cwd!()}/priv/certs/key.pem",
+    certfile: "#{File.cwd!()}/priv/certs/cert.pem"
   ],
   cors_proxy_url: [scheme: "https", host: cors_proxy_host, port: 4000],
   assets_url: [scheme: "https", host: assets_host, port: 4000],
